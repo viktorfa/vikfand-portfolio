228d61182db0075ec7fbac6a77b25cb2a757e436
viktorfa <vikfand@gmail.com>
2019-05-31 20:39:28 +0200

    Remove my name from Footer.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,22 +3,19 @@
     <hr>
     <g-link to="/#top">To the top</g-link>
     <div>
+      <code>{{commitSha}}</code>
+    </div>
+    <div>
       <span>
         {{year}} built with
         <a href="https://gridsome.org">Gridsome</a>
       </span>
     </div>
     <div>
-      <span>Viktor Andersen</span>
-    </div>
-    <div>
       <a href="https://blog.vikfand.com">Blog</a>
     </div>
     <div>
       <a href="https://travels.vikfand.com">Travel blog</a>
-    </div>
-    <div>
-      <code>{{commitSha}}</code>
     </div>
   </footer>
 </template>
