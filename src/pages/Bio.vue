dbff485d8ace3ca168c2a8e4ff4a00ac3115e7b0
viktorfa <vikfand@gmail.com>
2019-05-31 07:00:57 +0200

    Add bio page

---
+++ src/pages/Bio.vue
@@ -0,0 +1,14 @@
+<template>
+  <Layout>
+    <h1>Bio</h1>
+    <blockquote>Viktor is an amazing programmer and app developer from Norway. He has a extraordinary sense in understanding customer needs and identify valuable businesses. <br/> -- Bill Gates and Jeff Bezos</blockquote>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Bio"
+  }
+};
+</script>
